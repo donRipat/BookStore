382e023ece806286622767ac22dbfdd6322e1c52
donRipat <rafikov864@yandex.ru>
2022-10-18 12:28:19 +0500

    added genre, author and title filters

--- bookstore/main/views.py
+++ bookstore/main/views.py
@@ -7,19 +7,45 @@ from .models import Book
 
 
 def index(request):
-    books = Book.objects.all()
+    books = Book.objects.all().order_by('-bbe_score')
+    newlist = []
+    title = ''
+    author = ''
+    genre = ''
+    if request.method == 'POST':
+        title = request.POST['title']
+        author = request.POST['author']
+        genre = request.POST['genre']
+    for b in books:
+        if title in b.title and genre in b.genres and author in b.author:
+            newlist.append(b)
     strs = []
-    for i in range(Book.objects.count()):
+    for i in range(len(newlist)):
         if i % 3 == 0:
             strs.append([])
-        strs[i // 3].append(books[i])
+        strs[i // 3].append(newlist[i])
     print(strs)
-    return render(request, 'main/index.html', {'title': 'Главная', 'books': strs})
+    sign = 'Главная'
+    search = ''
+    if title != '' or author != '' or genre != '':
+        sign = ''
+        search = 'Результаты поиска '
+        if title != '':
+            search += f'в названиях: "{title}" '
+        if author != '':
+            search += f'среди авторов: "{author}" '
+        if genre != '':
+            search += f'среди жанров: "{genre}" '
+        search += f'({len(newlist)})'
+    return render(request, 'main/index.html', {'title': 'Главная', 'books': strs, 'sign': sign, 'search': search})
 
 
 def book_page(request, book_id):
     book = get_book_by_id(book_id)
     return render(request, 'main/book_page.html', {'book_page': book_page, 'book': book})
+
+
+#def logic (вынести сюда логику шапки и подвала)
 
 
 def get_book_by_id(book_id):
